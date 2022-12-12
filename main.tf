2d103d9472e248eee2b69029e4a9c1eb3026492b
dilrukjayawardena <dilrukjayawardena@users.noreply.github.com>
2022-12-12 04:46:42 +0000

    terraform formatted

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-       backend "remote" {
-        # The name of your Terraform Cloud organization.
-                      organization = "dush-organization"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "dush-organization"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-                        name = "dush-workspace"
-         }
-       }
-     }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "dush-workspace"
+    }
+  }
+}
 
-     # An example resource that does nothing.
-     resource "null_resource" "example" {
-                triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
