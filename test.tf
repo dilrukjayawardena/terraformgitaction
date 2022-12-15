5aefd8044ed51d713ce2cc6dedc97d565ec494bf
dilrukjayawardena <dushancst@gmail.com>
2022-12-15 15:14:32 +0530

    last4

--- test.tf
+++ test.tf
@@ -1,1 +1,12 @@
 #test1
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "dush-organization"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "dush-workspace"
+    }
+  }
+}
